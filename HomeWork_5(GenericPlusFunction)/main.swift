b67c85463f8f7748322d54dec8a43a58e01456ff
VladimirV3 <virushere@icloud.com>
2019-11-05 22:13:34 +0300

    Initial Commit

---
+++ HomeWork_5(GenericPlusFunction)/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HomeWork_5(GenericPlusFunction)
+//
+//  Created by Владимир Воронов on 11/5/19.
+//  Copyright © 2019 Vladimir Oleinikov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
